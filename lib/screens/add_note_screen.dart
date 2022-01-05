4b504ed582915967aa192c6ab43bdb2a2b33b596
Tolga <aygunestolgaa@gmail.com>
2022-01-05 15:51:50 +0300

    Flutter and GetX with SQFLite TODO application

---
+++ lib/screens/add_note_screen.dart
@@ -0,0 +1,279 @@
+import 'package:crud_sqlite_app/database/database.dart';
+import 'package:crud_sqlite_app/models/note_model.dart';
+import 'package:crud_sqlite_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AddNoteScreen extends StatefulWidget {
+  final Note ? note;
+  final Function ? updateNoteList;
+
+  AddNoteScreen({this.note,this.updateNoteList});
+
+  @override
+  _AddNoteScreenState createState() => _AddNoteScreenState();
+}
+
+class _AddNoteScreenState extends State<AddNoteScreen> {
+  final _formkey = GlobalKey<FormState>();
+
+  String _title = '';
+  DateTime _date = DateTime.now();
+  String _priority = 'Low';
+  String btnText = "Add Note";
+  String titleText = "Add Note";
+
+  TextEditingController _dateController = TextEditingController();
+
+  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+  final List<String> _priorities = ['Low', 'Medium', 'high'];
+
+  @override
+  void initState(){
+    super.initState();
+
+    if(widget.note != null){
+        _title = widget.note!.title!;
+        _date = widget.note!.date!;
+        _priority = widget.note!.priority!;
+
+        setState(() {
+          btnText = "Update note";
+          titleText = "Update note";
+        });
+    }
+    else{
+      setState(() {
+        btnText = "Add note";
+        titleText = "Add note";
+      });
+    }
+    _dateController.text = _dateFormatter.format(_date);
+  }
+
+  @override
+  void dispose(){
+    _dateController.dispose();
+    super.dispose();
+  }
+
+  _handleDatePicker() async {
+    final DateTime? date = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2050),
+    );
+    if(date != null && date != _date){
+       setState(() {
+         _date = date;
+       });
+       _dateController.text = _dateFormatter.format(date);
+    }
+  }
+
+  _submit(){
+
+    if(_formkey.currentState!.validate()){
+      _formkey.currentState!.save();
+      print('$_title, $_date, $_priority' );
+
+      Note note = Note(title: _title, date: _date, priority: _priority);
+
+      if(widget.note == null){
+        note.status = 0;
+        DatabaseHelper.instance.insertNote(note);
+
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (_) => HomeScreen(),
+            ),
+        );
+      }
+      else{
+        note.id = widget.note!.id;
+        note.status = widget.note!.status;
+        DatabaseHelper.instance.updateNote(note);
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => HomeScreen(),
+          ),
+        );
+      }
+      widget.updateNoteList!();
+    }
+  }
+
+  _delete(){
+    DatabaseHelper.instance.deleteNote(widget.note!.id!);
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (_) => HomeScreen(),
+      ),
+    );
+      widget.updateNoteList!();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => HomeScreen(),
+                      )),
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 30,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  titleText,
+                  style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                              labelText: 'Add Note',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                          ),
+                          validator: (input) =>
+                          input!.trim().isEmpty? 'Please enter a note title' : null,
+                          onSaved: (input) => _title = input!,
+                          initialValue: _title,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          controller: _dateController,
+                          style: TextStyle(fontSize: 18.0),
+                          onTap: _handleDatePicker,
+                          decoration: InputDecoration(
+                              labelText: 'Date',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              )),
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                        child: DropdownButtonFormField(
+                          isDense: true,
+                          icon: Icon(Icons.arrow_drop_down_circle),
+                          iconSize: 22.0,
+                          iconEnabledColor: Theme.of(context).primaryColor,
+                          items: _priorities.map((String priority){
+                              return DropdownMenuItem(
+                                value: priority,
+                                  child: Text(
+                                    priority,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                              );
+                          }).toList(),
+                            style: TextStyle(fontSize: 18.0),
+                            decoration: InputDecoration(
+                              labelText: 'Priority',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          validator: (input) => _priority ==null ? 'please select a priority level' : null,
+                          onChanged: (value){
+                            setState(() {
+                              _priority = value.toString();
+                            });
+                          },
+                          value: _priority,
+                          ),
+                        ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 20.0),
+                        height: 60,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: ElevatedButton(
+                          child: Text(
+                                  btnText,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0
+                                  ),
+                          ),
+                          onPressed: _submit ,
+                        ),
+                      ),
+                      widget.note != null ? Container(
+                        margin: EdgeInsets.symmetric(vertical: 20.0),
+                        height: 60.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(30.0)
+                        ),
+                        child: ElevatedButton(
+                          child: Text(
+                            'Date Note',
+                            style: TextStyle(
+                              color:Colors.white,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          onPressed: _delete,
+                        ),
+                      ): SizedBox.shrink(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,163 @@
+import 'package:crud_sqlite_app/database/database.dart';
+//import 'package:crud_sqlite_app/main.dart';
+import 'package:crud_sqlite_app/models/note_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'add_note_screen.dart';
+
+void main() => runApp(HomeScreen());
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  late BuildContext context;
+
+   late Future<List<Note>> _noteList;
+
+  final DateFormat _dateFormatter = DateFormat(' MMM  dd, yyyy') ;
+
+  DatabaseHelper _databaseHelper = DatabaseHelper.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateNoteList();
+  }
+
+
+
+  _updateNoteList() {
+    _noteList = DatabaseHelper.instance.getNoteList();
+  }
+
+
+
+  Widget _buildNote(Note note) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(note.title!,style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+                decoration: note.status == 0
+                    ? TextDecoration.none
+                    : TextDecoration.lineThrough,
+              ),
+            ),
+            subtitle: Text('${_dateFormatter.format(note.date!)} - ${note.priority}', style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.white,
+                decoration: note.status == 0
+                    ? TextDecoration.none
+                    : TextDecoration.lineThrough,
+              ),
+            ),
+            trailing: Checkbox(
+              onChanged: (value) {
+                note.status = value! ? 1 : 0 ;
+                DatabaseHelper.instance.updateNote(note);
+                _updateNoteList();
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+              },
+              activeColor: Theme.of(context).primaryColor,
+              value: note.status == 1 ? true:false,
+            ),
+            onTap: () => Navigator.push(context, CupertinoPageRoute(builder: (_) => AddNoteScreen(
+              updateNoteList: _updateNoteList(),
+              note: note,
+            ))),
+          ),
+          Divider(
+            height: 5.0,
+            color: Colors.deepPurple,
+            thickness: 2.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {
+          Navigator.push(
+              context,
+              CupertinoPageRoute(
+                builder: (_) => AddNoteScreen(
+                  updateNoteList: _updateNoteList,
+                ),
+              ));
+        },
+        child: Icon(Icons.add),
+      ),
+
+      body: FutureBuilder(
+
+        future: _noteList,
+
+       builder: (context,AsyncSnapshot snapshot) {
+
+          if(!snapshot.hasData || snapshot.data.documents.isEmpty){
+            return Center(
+              child: CircularProgressIndicator(
+                  color: Colors.black87
+              ),
+            );
+          }
+
+          final int completedNoteCount = snapshot.data!.where((Note note) => note.status == 1).toList().length;
+
+         return ListView.builder(
+             padding: EdgeInsets.symmetric(vertical: 80.0),
+             itemCount: int.parse(snapshot.data.lenth.toString()) + 1,
+             itemBuilder: (BuildContext context, int index) {
+               if (index == 0) {
+                 return Padding(
+                   padding: EdgeInsets.symmetric(
+                       horizontal: 40.0, vertical: 20),
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: [
+                       Text(
+                         "My Notes",
+                         style: TextStyle(
+                             color: Colors.deepPurple,
+                             fontSize: 40,
+                             fontWeight: FontWeight.bold),
+                       ),
+                       SizedBox(
+                         height: 10,
+                       ),
+                       Text(
+                         '$completedNoteCount of ${snapshot.data.length}',
+                         style: TextStyle(
+                           color: Colors.blueAccent,
+                           fontSize: 20,
+                           fontWeight: FontWeight.w600,
+                         ),
+                       ),
+                     ],
+                   ),
+                 );
+               }
+
+               return _buildNote(snapshot.data![index - 1]);
+             }
+             );
+       }
+    ),
+    );
+  }
+}
